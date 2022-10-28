2d3c636e331b344131c476b0ba18304c7290c2d5
ingrid <igvillela@indeed.com>
2022-10-28 08:58:38 -0700

    added two cases

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import { PackageURL } from 'packageurl-js';
 
-const pkg = new PackageURL(
-  'maven',
-  'org.springframework.integration',
-  'spring-integration-jms',
-  '5.5.5',
+
+const pkg1 = new PackageURL(
+  'npm',
+  '@babel/runtime-corejs3',
+  'babel',
+  '7.12.1',
+  undefined,
+  undefined
+);
+//npm		@typescript-eslint/type-utils
+const pkg2 = new PackageURL(
+  'go',
+  'github.com/d2g',
+  'dhcp4client',
+  '2.5.5',
   undefined,
   undefined
 );
 
-console.log(pkg.toString());
+console.log(`This should have @ instead of %40 \n${pkg1.toString()}\n`);
+console.log(`This should have golang instead of go \n${pkg2.toString()}\n`);
+
+
