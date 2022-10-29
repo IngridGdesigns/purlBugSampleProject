266e86e99721f8f06c801e3710c7d6d9af4d47c0
ingrid <igvillela@indeed.com>
2022-10-28 22:27:54 -0700

    added other cases and an observation

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { PackageURL } from 'packageurl-js';
 
-
 const pkg1 = new PackageURL(
   'npm',
   '@babel/runtime-corejs3',
@@ -9,17 +8,38 @@ const pkg1 = new PackageURL(
   undefined,
   undefined
 );
-//npm		@typescript-eslint/type-utils
+
 const pkg2 = new PackageURL(
+  'pypi',
+  'PyMySQL',
+  'pymysql',
+  '0.9.1',
+  undefined,
+  undefined
+);
+
+const pkg3 = new PackageURL(
   'go',
-  'github.com/d2g',
-  'dhcp4client',
-  '2.5.5',
+  'github.com',
+  'hashicorp/go-getter',
+  '2.0.2',
+  undefined,
+  undefined
+);
+
+const pkg4 = new PackageURL(
+  'pypi',
+  'Appium-Python-Client',
+  ' ',
+  '2.2.0',
   undefined,
   undefined
 );
 
 console.log(`This should have @ instead of %40 \n${pkg1.toString()}\n`);
-console.log(`This should have golang instead of go \n${pkg2.toString()}\n`);
+console.log(`checking if name was forced to lowercase \n${pkg2.toString()}\n`);
+console.log(`This should have '/' instead of %2F\n${pkg3.toString()}\n`);
 
-
+const observation =
+  'This errors if I leave an empty string, had to add whitespace to get %20\n';
+console.log(`${observation}${pkg4.toString()}\n`); // more of an observation
